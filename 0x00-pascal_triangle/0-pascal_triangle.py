d92ead86b2e47ed7707dd876acfa8d844564a545
Smolclin <clinsmol10@gmail.com>
2024-10-03 17:30:59 +0000

    Correct errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,21 +5,31 @@ pascal's triangle
 
 """
 
+"""
+    Create a function that returns
+    a list of lists of integers
+    representing the Pascal’s triangle of n
+"""
+
 def pascal_triangle(n):
     """
-    Create a function that returns 
+    Create a function that returns
     a list of lists of integers
     representing the Pascal’s triangle of n
     """
-    k = []
+
+    new_pascal = []
+
+    """ Assumes that n is an integer """
     if n <= 0:
-        return k
-    k = [[1]]
-    for i in range(1, n):
-        temp = [1]
-        for j in range(len(k[i - 1]) - 1):
-            curr = k[i - 1]
-            temp.append(k[i - 1][j] + k[i - 1][j + 1])
-        temp.append(1)
-        k.append(temp)
-    return k
+        return new_pascal
+
+    for i in range(n):
+        row_index = [1]
+        if new_pascal:
+            final_row = new_pascal[-1]
+            row_index.extend([sum(pair) for pair in
+                              zip(final_row, final_row[1:])])
+            row_index.append(1)
+        new_pascal.append(row_index)
+    return (new_pascal)
