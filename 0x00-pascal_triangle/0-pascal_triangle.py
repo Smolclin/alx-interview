d4d032d2d560be5ef2f6d4b4a37f5b6e7d5828b3
Smolclin <clinsmol10@gmail.com>
2024-10-03 17:35:25 +0000

    Correct errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,13 +5,9 @@ pascal's triangle
 
 """
 
-"""
-    Create a function that returns
-    a list of lists of integers
-    representing the Pascal’s triangle of n
-"""
 
 def pascal_triangle(n):
+
     """
     Create a function that returns
     a list of lists of integers
